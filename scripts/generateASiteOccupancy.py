0e32a265989fd5f1e42e0646045f9435129e0478
RickGelhausen <rick.gelhausen@gmail.com>
2019-04-26 13:58:37 +0200

    added coverage

--- scripts/generateASiteOccupancy.py
+++ scripts/generateASiteOccupancy.py
@@ -6,12 +6,18 @@ import sys
 
 class generateASiteOccupancy:
 
-    def __init__(self, alignment_file, antisense_out, sense_out):
+    def __init__(self, alignment_file, prefix):
         self.alignment_file = alignment_file # sam / bam format
+
+        self.cov_s_dict = {}
+        self.cov_as_dict = {}
         self.a_site_s_dict = {}
         self.a_site_as_dict = {}
-        self.antisense_out = antisense_out
-        self.sense_out = sense_out
+
+        self.cov_sense_out = prefix + "_cov_fwd.bedgraph"
+        self.cov_antisense_out = prefix + "_cov_rev.bedgraph"
+        self.asite_sense_out = prefix + "_asite_fwd.bedgraph"
+        self.asite_antisense_out = prefix + "_asite_rev.bedgraph"
 
         self._fill_dictionary()
 
@@ -19,42 +25,78 @@ class generateASiteOccupancy:
         """
         writes the contents of dictionary to file
         """
-        with open(self.sense_out, "w") as of:
+        with open(self.cov_sense_out, "w") as of:
+            # go through every entry in the dictionary
+            for key, val in self.cov_s_dict.items():
+                # write line to file
+                of.write("%s\t%s\t%s\t%s\n" % (key[0], key[1], key[2], val))
+
+        with open(self.cov_antisense_out, "w") as of:
+            # go through every entry in the dictionary
+            for key, val in self.cov_as_dict.items():
+                # write line to file
+                of.write("%s\t%s\t%s\t%s\n" % (key[0], key[1], key[2], val))
+
+        with open(self.asite_sense_out, "w") as of:
             # go through every entry in the dictionary
             for key, val in self.a_site_s_dict.items():
                 # write line to file
                 of.write("%s\t%s\t%s\t%s\n" % (key[0], key[1], key[2], val))
 
-        with open(self.antisense_out, "w") as of:
+        with open(self.asite_antisense_out, "w") as of:
             # go through every entry in the dictionary
             for key, val in self.a_site_as_dict.items():
                 # write line to file
                 of.write("%s\t%s\t%s\t%s\n" % (key[0], key[1], key[2], val))
 
-    def _a_site_occupancy(self, flag, region, start, length):
+    def _a_site_occupancy(self, flag, region, start):
         """
         calculate the a-site occupancy
         """
         if flag == 0:
-            a_site_start = start + 12
-            a_site_stop = a_site_start + 1
+            pow = 1
+        elif flag == 16
+            pow = -1
+        else:
+            return
 
-            entry = (region, a_site_start, a_site_stop)
+        a_site_start = start + pow * 12
+        a_site_stop = a_site_start + pow
+
+        entry = (region, a_site_start, a_site_stop)
+        if flag == 0:
             if entry in self.a_site_s_dict:
                 self.a_site_s_dict[entry] += 1
             else:
                 self.a_site_s_dict[entry] = 1
-
-
-        elif flag == 16:
-            a_site_start = start - 12
-            a_site_stop = a_site_start - 1
-
-            entry = (region, a_site_start, a_site_stop)
-            if entry in self.a_site_s_dict:
+        elif flag == 16
+            if entry in self.a_site_as_dict:
                 self.a_site_as_dict[entry] += 1
             else:
                 self.a_site_as_dict[entry] = 1
+
+
+    def _coverage(self, flag, region, start, length):
+        """
+        calculate the coverage
+        """
+        if flag == 0:
+            cov_stop = start + length - 1
+            for step in range(start, cov_stop + 1):
+                entry = (region, step, step + 1)
+                if entry in self.cov_s_dict:
+                    self.cov_s_dict[entry] += 1
+                else:
+                    self.cov_s_dict[entry] = 1
+
+        elif flag == 16:
+            cov_stop = start - length + 1
+            for step in range(cov_stop, start + 1):
+                entry = (region, step, step + 1)
+                if entry in self.cov_as_dict:
+                    self.cov_as_dict[entry] += 1
+                else:
+                    self.cov_as_dict[entry] = 1
 
     def _fill_dictionary(self):
         """
@@ -69,7 +111,8 @@ class generateASiteOccupancy:
             reference_pos = read.pos + 1 # 0- based leftmost mapping position (reference_pos)
             read_length = len(read.query_sequence)
 
-            self._a_site_occupancy(flag, reference_name, reference_pos, read_length)
+            self._a_site_occupancy(flag, reference_name, reference_pos)
+            self._coverage(flag, reference_name, reference_pos, read_length)
 
         samfile.close()
         self._write_output()
@@ -83,10 +126,7 @@ def main():
                         , help="the prefix for the output file.")
     args = parser.parse_args()
 
-    sense_out = args.output_prefix + "_asite_fwd.bedgraph"
-    antisense_out = args.output_prefix + "_asite_rev.bedgraph"
-
-    gBG = generateASiteOccupancy(args.alignment_file, antisense_out, sense_out)
+    gBG = generateASiteOccupancy(args.alignment_file, args.output_prefix)
 
 
 if __name__ == '__main__':
