41490b5a2dec9fec12d982302efab2d4ec9664fc
muellert <teresa-m@t-online.de>
2020-02-28 16:57:40 +0100

    typos fix

--- evaluation/statistics_pos_neg.py
+++ evaluation/statistics_pos_neg.py
@@ -14,7 +14,7 @@ import glob
 
 
 #filter on
-
+# 1000000000 as cutoff is higher than all scores the tools would produce! Therefore the score_cutoff will be not used!
 score_cutoff = 1000000000
 
 
@@ -55,14 +55,14 @@ def get_dict_with_empty_val(file, tool='RefSeq'):
     return tool_dict
 
 
-def get_assosiated_genes(gene_dict, pred_predition_dict, label):
+def get_associated_genes(gene_dict, pred_prediction_dict, label):
     """function assosiates just one prediction to a gene.
 
         Parameters
         ----------
         gene_dict : dictionary
             geneIds with list of predictions
-        pred_predition_dict : dictionary
+        pred_prediction_dict : dictionary
             predictionIds with lists of genes
 
         Raises
@@ -71,28 +71,24 @@ def get_assosiated_genes(gene_dict, pred_predition_dict, label):
 
         Returns
         -------
-        assosiated_genes_dict
-            assositation of geneId -> preditionId (one to one relation)
-        pred_predition_sub_dict
-            containing predictionsIds that are not assosiated to a gene
-        assosiated_predictions_dict
-            assositation of preditionId -> geneId (one to one relation)
+        associated_genes_dict
+            assositation of geneId -> predictionId (one to one relation)
+        pred_prediction_sub_dict
+            containing predictionsIds that are not associated to a gene
+        associated_predictions_dict
+            assositation of predictionId -> geneId (one to one relation)
     """
-    assosiated_genes_dict = {}
-    assosiated_predictions_dict = {}
-    pred_predition_sub_dict = pred_predition_dict
+    associated_genes_dict = {}
+    associated_predictions_dict = {}
+    pred_prediction_sub_dict = pred_prediction_dict
     score_list = []
     fn = 0
-    #[print(k,v) for (k,v) in pred_predition_dict.items()]
-    #print('\n')
-    #[print(k,v) for (k,v) in gene_dict.items()]
-    #print('\n')
 
     for k in gene_dict:
-        #print(k)
+
         # sort list of predictions according to there score
         sorted_list = sorted(gene_dict[k], key=itemgetter(2), reverse=True)
-        #print(sorted_list)
+
         i = 0
         # find gene:
         while len(sorted_list) != 0:
@@ -100,33 +96,32 @@ def get_assosiated_genes(gene_dict, pred_predition_dict, label):
             id_pred = pred_info_list[0]
             score = pred_info_list[1]
             overlap = pred_info_list[2]
-            #print(id_pred)
-            #print(pred_predition_sub_dict.keys())
-            if id_pred in pred_predition_sub_dict.keys():
+
+            if id_pred in pred_prediction_sub_dict.keys():
                 #print(id_pred)
                 try:
-                    del pred_predition_sub_dict[id_pred]
+                    del pred_prediction_sub_dict[id_pred]
                 except KeyError:
                     pass
-                assosiated_genes_dict[k] = id_pred
-                assosiated_predictions_dict[id_pred]= k
+                associated_genes_dict[k] = id_pred
+                associated_predictions_dict[id_pred]= k
                 score_list.append((float(score), overlap, label))
 
                 sorted_list = []
             elif sorted_list == []:
                 fn += 1
                 #score_list.append((0,0,label))
-                #print(assosiated_genes_dict)
-        #print(pred_predition_sub_dict)
+                #print(associated_genes_dict)
+        #print(pred_prediction_sub_dict)
 
 
-    return (assosiated_genes_dict, pred_predition_sub_dict,
-            assosiated_predictions_dict, fn, score_list)
+    return (associated_genes_dict, pred_prediction_sub_dict,
+            associated_predictions_dict, fn, score_list)
 
 
 def get_genes(gene_dict, df_overlap_score, tool):
     """funciton selects for a saves tool all predicted genes (key) and there
-    assosiated predictions (value) in a dictionay
+    associated predictions (value) in a dictionay
 
         Parameters
         ----------
@@ -143,7 +138,7 @@ def get_genes(gene_dict, df_overlap_score, tool):
 
         Returns
         -------
-        gene_predition_dict
+        gene_prediction_dict
             A dictionary: {'geneId1': [['predictionId1', 'score', 'overlap'],
             ['predictionId2', 'score', 'overlap']...] 'geneId2':
             [['predictionId3', 'score', 'overlap'],...]...}
@@ -166,31 +161,31 @@ def get_genes(gene_dict, df_overlap_score, tool):
     #print(df_overlap_test)
 
     # dict key: gene_id; value: list of overlaping prediction (predID$score$overlap)
-    gene_predition_temp_dict = {k: list(v)
+    gene_prediction_temp_dict = {k: list(v)
                                 for k, v in df_overlap_test.groupby(
                                 by=['start_ref', 'stop_ref', 'strand_ref', 'chr'])
                                 ['tuple']}
-    #print(gene_predition_temp_dict)
+    #print(gene_prediction_temp_dict)
 
     # making a sting out of the tuple saved as key
-    gene_predition_dict ={str(k[0]) + ':' + str(k[1]) + ':' +
-                          k[2] + ':' + k[3]: gene_predition_temp_dict[k]
-                          for k in gene_predition_temp_dict}
+    gene_prediction_dict ={str(k[0]) + ':' + str(k[1]) + ':' +
+                          k[2] + ':' + k[3]: gene_prediction_temp_dict[k]
+                          for k in gene_prediction_temp_dict}
 
-    for gene_id in gene_predition_dict:
-        gene_predition_dict[gene_id] = [val.split('$')
-                                        for val in gene_predition_dict
+    for gene_id in gene_prediction_dict:
+        gene_prediction_dict[gene_id] = [val.split('$')
+                                        for val in gene_prediction_dict
                                         [gene_id]]
 
     # compute what genes are not predicted
-    # not_pred_dict = gene_dict.keys() - gene_predition_dict.keys()
+    # not_pred_dict = gene_dict.keys() - gene_prediction_dict.keys()
 
-    return gene_predition_dict
+    return gene_prediction_dict
 
 
 def get_prediction(tool_dict, df_overlap_score, tool):
     """This fuction selects for a given tool all predictions (key) and there
-    assosiated genes (value) and stores them in a dictionay
+    associated genes (value) and stores them in a dictionay
 
         Parameters
         ----------
@@ -207,7 +202,7 @@ def get_prediction(tool_dict, df_overlap_score, tool):
 
         Returns
         -------
-        pred_predition_dict
+        pred_prediction_dict
             A dictionary: {'predId1': [['geneId1', 'overlap'],
             ['geneId2', ' 'overlap']...] 'predId2':
             [['geneId3', 'overlap'],...]...}
@@ -227,25 +222,25 @@ def get_prediction(tool_dict, df_overlap_score, tool):
                                 df_overlap_test['overlap_percent'])
 
     # generate gene_prediction dataframe
-    pred_predition_temp_dict = {pred_Id: list(gene_ids) for pred_Id, gene_ids in
+    pred_prediction_temp_dict = {pred_Id: list(gene_ids) for pred_Id, gene_ids in
                                 df_overlap_test.groupby(by=['start_pred',
                                 'stop_pred', 'strand_pred', 'chr'])
                                 ['tuple']}
     # convertd pred_id tuple into one string
-    pred_predition_dict = {str(k[0]) + ':' + str(k[1]) + ':' +
-                           k[2] + ':' + k[3]: pred_predition_temp_dict[k]
-                           for k in pred_predition_temp_dict}
+    pred_prediction_dict = {str(k[0]) + ':' + str(k[1]) + ':' +
+                           k[2] + ':' + k[3]: pred_prediction_temp_dict[k]
+                           for k in pred_prediction_temp_dict}
 
 
-    for k in pred_predition_dict:
+    for k in pred_prediction_dict:
         # sprlit gene informatio stroed in values into gene_id and overlap_percent
-        pred_predition_dict[k] = [val.split('-')
-                                  for val in pred_predition_dict[k]]
+        pred_prediction_dict[k] = [val.split('-')
+                                  for val in pred_prediction_dict[k]]
 
     # compute what genes are not predicted
-    #not_pred_dict = tool_dict.keys() - pred_predition_dict.keys()
+    #not_pred_dict = tool_dict.keys() - pred_prediction_dict.keys()
 
-    return pred_predition_dict
+    return pred_prediction_dict
 
 
 def assort_genes_with_predictions(ref_file, tools_file, df_overlap_score, tool, label):
@@ -276,57 +271,47 @@ def assort_genes_with_predictions(ref_file, tools_file, df_overlap_score, tool, 
             number of genes without predictiontools_file
         suboptimals
             number of suboptimal predictions
-        assosiated_gene_list
+        associated_gene_list
             list of all genes found by the currently investigated tool
         score_list
-            list contining for the current tool (score, overlap, label) for each assosiated gene
+            list contining for the current tool (score, overlap, label) for each associated gene
 
     """
     # put all genes of the refernece inside a dictionary
     gene_dict = get_dict_with_empty_val(ref_file)
     print('number of genes for %s: %i' % (label, len(gene_dict)))
-    #print(len(gene_dict))
-    #print('\n')
+
     # put all predictions inside a dictionary
     tool_dict = get_dict_with_empty_val(tools_file, tool=tool)
-    #print(tool_dict)
-    #print('\n')
+
     # get all genes which are found by the tools prediction and the genes that are not found!
     gene_predicted_dict = get_genes(gene_dict, df_overlap_score, tool)
-    #print(df_overlap_score)
-    #print(len(gene_predicted_dict))
-    #print(list(gene_predicted_dict.keys()))
-    #print(list(gene_predicted_dict.values())[0])
-    #print('\n')
-    # get all predicitons of the tools that have a overlap with a gene of the reference
-    predition_overlap_dict = get_prediction(tool_dict, df_overlap_score, tool)
-    #print(len(predition_overlap_dict))
-    #print('\n')
-    # assosiat each gene with just one prediciton. All othere genes
-    assosiated_genes_dict, pred_predition_sub_list, assosiated_predictions_dict, no_genes_lost_prediction, score_list = get_assosiated_genes(gene_predicted_dict, predition_overlap_dict,label)
-    #print(len(assosiated_genes_dict))
-    #print('\n')
-    #print(pred_predition_sub_list)
-    #print('\n')
 
-    assert (len(assosiated_genes_dict) == len(assosiated_predictions_dict)),\
+    # get all predicitons of the tools that have a overlap with a gene of the reference
+    prediction_overlap_dict = get_prediction(tool_dict, df_overlap_score, tool)
+
+    # assosiat each gene with just one prediciton. All othere genes
+    associated_genes_dict, pred_prediction_sub_list, associated_predictions_dict, no_genes_lost_prediction, score_list = get_associated_genes(gene_predicted_dict, prediction_overlap_dict,label)
+
+
+    assert (len(associated_genes_dict) == len(associated_predictions_dict)),\
             "list of overlapping genes and prediction is not equal"
 
-    if not len(assosiated_genes_dict):
+    if not len(associated_genes_dict):
         assorted_genes = 0
         not_assorted_genes = len(gene_dict)
     else:
-        assorted_genes = len(assosiated_predictions_dict)
-        not_assorted_genes = len(gene_dict) - len(assosiated_predictions_dict)
+        assorted_genes = len(associated_predictions_dict)
+        not_assorted_genes = len(gene_dict) - len(associated_predictions_dict)
 
-    if not len(pred_predition_sub_list):
+    if not len(pred_prediction_sub_list):
         suboptimals = 0
     else:
-        suboptimals = len(pred_predition_sub_list)
+        suboptimals = len(pred_prediction_sub_list)
 
-    assosiated_gene_list =  list(assosiated_genes_dict.keys())
+    associated_gene_list =  list(associated_genes_dict.keys())
 
-    return assorted_genes, not_assorted_genes, suboptimals, assosiated_gene_list, score_list
+    return assorted_genes, not_assorted_genes, suboptimals, associated_gene_list, score_list
 
 
 def get_stat_for_tool(ref_file, tools_file, df_overlap_score, ref_neg_file, df_neg_overlap, df_fp_overlap, flag_subopt, flag_no_gene, tool, save_dir):
@@ -363,8 +348,8 @@ def get_stat_for_tool(ref_file, tools_file, df_overlap_score, ref_neg_file, df_n
         FN_prediction_list
             list of all not predicted genes
     """
-    TP, FN, suboptimals_TP, assosiated_TP_list, score_list_pos = assort_genes_with_predictions(ref_file, tools_file, df_overlap_score, tool, '1')
-    FP, TN, suboptimals_FP, assosiated_FP_list, score_list_neg = assort_genes_with_predictions(ref_neg_file, tools_file, df_neg_overlap, tool, '0')
+    TP, FN, suboptimals_TP, associated_TP_list, score_list_pos = assort_genes_with_predictions(ref_file, tools_file, df_overlap_score, tool, '1')
+    FP, TN, suboptimals_FP, associated_FP_list, score_list_neg = assort_genes_with_predictions(ref_neg_file, tools_file, df_neg_overlap, tool, '0')
     #print(score_list_pos)
 
     # get list for ROC curve containing (score, overlap, label)
@@ -437,10 +422,10 @@ def get_stat_for_tool(ref_file, tools_file, df_overlap_score, ref_neg_file, df_n
         specificity = 0
 
 
-    # #if not pred_predition_sub_list:
+    # #if not pred_prediction_sub_list:
     #     #subset = 0
     # #else:
-    # suboptimals = len(pred_predition_sub_list)
+    # suboptimals = len(pred_prediction_sub_list)
     print("+++++++\nTOOL: %s, SUBOPTIMALS: %s\n" % (tool, suboptimals_TP))
     print('TP '+ str(TP) +'\n')
     print('FP '+ str(FP) +'\n')
@@ -451,11 +436,11 @@ def get_stat_for_tool(ref_file, tools_file, df_overlap_score, ref_neg_file, df_n
     print('predictions not in genes '+ str(no_prediction_without_genes) +'\n+++++++++\n')
     no_prediction_without_genes
     #
-    # gene_list = assosiated_genes_dict.keys()
-    # prediction_list = assosiated_predictions_dict.keys()
+    # gene_list = associated_genes_dict.keys()
+    # prediction_list = associated_predictions_dict.keys()
     stat_list = [TP, TN, FP, FN, recall, specificity, FNR, precision, FDR, F1, accuracy,
                  suboptimals_TP, suboptimals_FP, no_prediction_without_genes, tool]
-    return stat_list, assosiated_TP_list
+    return stat_list, associated_TP_list
     # return (gene_list, prediction_list, stat_list,
     #         FP_prediction_list, FN_prediction_list)
 
@@ -562,7 +547,7 @@ def call_bedtools_not_overlap(reference_path, tools_path, overlap_cutoff, overla
 
     # test if gtf file is emty:
     if os.stat(overlap_file).st_size == 0:
-        print('**********************\nno prediction exitst that could not be assosiated with any gene\n**********************\n')
+        print('**********************\nno prediction exitst that could not be associated with any gene\n**********************\n')
         df_overlap_score = score_cutoff
     else:
         df_temp = pd.read_csv(overlap_file, header=None, sep="\t", comment="#")
